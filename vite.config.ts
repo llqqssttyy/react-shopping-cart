b8782c9a8c907814e253bac039a165a1a6faf43c
Kim Da Eun <midekuna@gmail.com>
2024-05-16 17:14:46 +0900

    chore: step1 빌드

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      web3: "web3/dist/web3.min.js",
+      web3: 'web3/dist/web3.min.js',
     },
   },
+  base: '/react-shopping-cart/',
 });
